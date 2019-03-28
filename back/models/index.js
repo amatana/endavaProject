467a9e554432fc85384a1ecd6a633b111bd89769
gastondh91 <gastondh91@gmail.com>
2019-03-28 16:31:45 -0300

    cambios menores

--- back/models/index.js
+++ back/models/index.js
@@ -1,9 +1,14 @@
 const User = require('./User');
 const Interview = require('./interview');
+const Questions = require('./questions');
+const Tags = require('./tags');
 
 User.belongsTo(Interview, { as: "userHR"});
 User.belongsTo(Interview, { as: "userSis1"});
 User.belongsTo(Interview, { as: "userSis2"});
+
+Questions.belongsToMany(Tags, {through: 'tags-questions'})
+Tags.belongsToMany(Questions, {through: 'tags-questions'})
 
 module.exports = {
     User,
