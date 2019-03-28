cfa9f284e7ae526e8be0d9a760301a2c5e726a7b
amatana <amatana95@gmail.com>
2019-03-28 14:47:40 -0300

    testeando

---
+++ back/models/User.js
@@ -0,0 +1,53 @@
+const db = require('../config/db');
+const S = require('sequelize');
+const crypto = require('crypto');
+
+const User = db.define('usuario', {
+  nombre: {
+    type: S.STRING,
+    allowNull: false
+  },
+  apellido: {
+    type: S.STRING,
+    allowNull: false
+  },
+  email: {
+    type: S.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  area: {
+    type: S.ENUM,
+    values: ["RRHH", "Sistemas"],
+    allowNull: false
+  },
+  password: {
+    type: S.STRING,
+    allowNull: false
+  },
+  salt: {
+    type: S.STRING
+  },
+  isAdmin: {
+    type: S.BOOLEAN
+  }
+});
+
+Usuario.addHook('beforeCreate', (usuario) => {
+  usuario.salt = crypto.randomBytes(20).toString('hex');
+  usuario.password = usuario.hashPassword(usuario.password);
+});
+
+
+Usuario.prototype.hashPassword = function (password) {
+  return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+};
+
+Usuario.prototype.validPassword = function (password) {
+  return this.password === this.hashPassword(password);
+};
+
+module.exports = User;
