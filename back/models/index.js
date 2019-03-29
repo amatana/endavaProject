5241c2266f2b2a6997acc2464c305030a71cdfd8
LeoCarrocio <leocarrocio@gmail.com>
2019-03-29 12:39:19 -0300

    tabla answers

--- back/models/index.js
+++ back/models/index.js
@@ -1,11 +1,21 @@
 const User = require('./User');
-const Interview = require('./interview');
+const Interview = require('./Interview');
+const Answers = require ('./answers')
 
 User.belongsTo(Interview, { as: "userHR"});
 User.belongsTo(Interview, { as: "userSis1"});
 User.belongsTo(Interview, { as: "userSis2"});
 
+
+Interview.belongsTo(Answers,{as:'interviewID'})
+Questions.belongsTo(Answers,{as:'questionsID'})
+
+
 module.exports = {
     User,
-    Interview
+    Interview,
+    Questions,
+    Answers
+    
+    
 }
