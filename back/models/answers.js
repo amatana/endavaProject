aafae76badc9dbe4c3ae839602a0a089b10a1472
LeoCarrocio <leocarrocio@gmail.com>
2019-03-29 12:49:31 -0300

    tabla answers

--- back/models/answers.js
+++ back/models/answers.js
@@ -1,14 +1,13 @@
 const S = require('sequelize');
 const db = require('../config/db');
 
-const Answers= db.define('answers', {
-    observations: {
-      type: S.TEXT,
-    },
-    punctuation: {
-      type: S.INTEGER,
-    },
+const Answers = db.define('answers', {
+  observations: {
+    type: S.TEXT
+  },
+  punctuation: {
+    type: S.INTEGER
+  }
 });
-  
-  module.exports = Answers;
-  
+
+module.exports = Answers;
