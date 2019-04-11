0109daa4b76da49cf3c49b9b886420b0796f9407
gastondh91 <gastondh91@gmail.com>
2019-04-11 11:07:09 -0300

    fixes implementados

--- front/src/containers/seed.js
+++ front/src/containers/seed.js
@@ -1,4 +1,4 @@
-export const candidatos = [{ candidato: 'Jesu Dio', perfil: 'Junior', estado: 'Approbed HR' },
+export const preguntas = [{ candidato: 'Jesu Dio', perfil: 'Junior', estado: 'Approbed HR' },
   { candidato: 'Cristian U', perfil: 'Senior', estado: 'Rejected HR' }, { candidato: 'Lord Farquad',
     perfil: 'Semi-Senior',
     estado: 'Tech Approved' }, { candidato: 'Silvia Suller',
@@ -10,9 +10,3 @@ export const candidatos = [{ candidato: 'Jesu Dio', perfil: 'Junior', estado: 'A
     estado: 'New' }, { candidato: 'Angel',
     perfil: 'Semi-Senior',
     estado: 'Approbed HR' }];
-
-export const entrevistadoresSist = ['Gaston d' + 'Hiriart', 'Ana Amat', 'Leonardo Carrocio', 'Joaquin Quiroga', 'Juan Manuel']
-;
-
-export const entrevistadoresHr = ['Maria Elena Walsh', 'Virginia Lagos', 'Cristo Jesu', 'Profesor Jirafales', 'Pep Guardiola']
-;
