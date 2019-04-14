35884ec81311d3771fa3945d314c3fd9d85cf0e9
gastondh91 <gastondh91@gmail.com>
2019-04-14 16:09:28 -0300

    sin cambios

--- front/src/components/CreateInterviewComp.jsx
+++ front/src/components/CreateInterviewComp.jsx
@@ -4,29 +4,33 @@ import React from 'react';
 
 const CreateInterviewComp = (props) => (
   <div>
-    <div className='probando'>
-      <div style={{marginLeft:'30px'}}>
-        <h2>Full name:</h2> {props.candidate.fullName}
-        <h2>Phone:</h2> {props.candidate.telNumber}
-        <h2>Email Adress:</h2> {props.candidate.email}
-      </div>
+    <div>
+      <div className='probando'>
+        <div style={{ marginLeft: '30px' }}>
+          <h2>Full name:</h2> {props.candidate.fullName}
+          <h2>Phone:</h2> {props.candidate.telNumber}
+          <h2>Email Adress:</h2> {props.candidate.email}
+        </div>
 
-      <div >
-        <h2>Tags:</h2> {'aca van los tags'}
+        <div >
+          <h2>Tags:</h2> {'aca van los tags'}
+        </div>
       </div>
     </div>
 
-    <form id='formCand'>
-      {props.questions.map((pregunta) => (
-        <div key={pregunta.id} className="form-row">
-          <div className="form-group col-md-6">
-            <label htmlFor="inputEmail4" style={{ margin: '10px' }}>{pregunta.content}</label>
-            <textarea onChange={props.onChange} name={ pregunta.id }style={{ margin: '5px' }} type="email" className="form-control" id="inputEmail4" placeholder="Respuesta"/>
+    <div>
+      <form id='formCand'>
+        {props.questions.map((pregunta) => (
+          <div key={pregunta.id} className="form-row">
+            <div className="form-group col-md-6">
+              <label htmlFor="inputEmail4" style={{ margin: '10px' }}>{pregunta.content}</label>
+              <textarea onChange={props.onChange} name={pregunta.id} style={{ margin: '5px' }} type="email" className="form-control" id="inputEmail4" placeholder="Respuesta" />
+            </div>
           </div>
-        </div>
-      ))}
-      <button type='button' className='ActionsBotonesNaranja btn-lg' onClick ={props.onSubmit}>Submit</button>
-    </form>
+        ))}
+        <button type='button' className='ActionsBotonesNaranja btn-lg' onClick={props.onSubmit}>Submit</button>
+      </form>
+    </div>
   </div>
 );
 
