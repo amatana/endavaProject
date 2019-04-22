471e7f467db49e8606e4214a06bd19292e73cc0f
amatana <amatana95@gmail.com>
2019-04-22 04:14:11 -0300

    solucionado

--- front/src/containers/PreSistInterview.jsx
+++ front/src/containers/PreSistInterview.jsx
@@ -8,63 +8,76 @@ import { fetchCandidate } from '../redux/action-creator/candidate-actions';
 import { fetchCandidateQuestions } from '../redux/action-creator/questionActions';
 
 class PreSistInterview extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.state = {
       candidato: {
         tags: []
-      },
-      submited: false
+      }
     };
     this.onChangeCheckbox = this.onChangeCheckbox.bind(this);
     this.onClickFunc = this.onClickFunc.bind(this);
   };
 
-  componentDidMount() {
+  componentDidMount () {
     this.props.fetchCandidate(this.props.candID);
   }
 
-  componentDidUpdate(prevState) {
+  componentDidUpdate (prevState) {
     if (prevState.candidate !== this.props.candidate) {
       this.props.fetchCandidateQuestions(this.props.candidate.tags);
       this.setState({ candidato: this.props.candidate });
     }
   }
 
-  onChangeCheckbox(e) {
+  onChangeCheckbox (e) {
     e.preventDefault();
     let arrayQuestionsID = [];
-    // console.dir(e.target);
+    console.dir(e.target);
     for (let i = 0; i < this.props.candidateQuestions.length; i += 1) {
-      // console.log(e.target[i].value, '++++++++++ ', e.target[i].checked);
+      console.log(e.target[i].value, '++++++++++ ', e.target[i].checked);
       if (e.target[i].checked) arrayQuestionsID.push(Number(e.target[i].value));
     }
-    // console.log('arreglo de ID de pregutnas', arrayQuestionsID);
-    // console.log('CANDIDATO', this.props.candidate.id);
+    console.log('arreglo de ID de pregutnas', arrayQuestionsID);
+    console.log('CANDIDATO', this.props.candidate.id);
     this.props.setInterviewCandidate({
       candidateID: this.props.candidate.id,
       questionsID: arrayQuestionsID
     });
     alert('Interview has been created! ');
+    this.props.history.push(`/candidates/${this.props.candidate.id}`);
   }
 
-  onClickFunc() {
+  onClickFunc () {
     this.props.history.push(`/candidates/interviewSis/${this.props.candidate.id}`);
   }
-  render() {
+  render () {
     return (<div>
       <img src="/utils/Frame.png"></img>
       {/* <div>
-        <h2>Candidate info
+        <h2 style={{ position: 'absolute',
+          width: '325px',
+          height: '22px',
+          left: '40px',
+          top: '155px',
+
+          fontFamily: 'Roboto Condensed',
+          fontStyle: 'normal',
+          fontWeight: 'bold',
+          fontSize: '20px',
+          lineHeight: 'normal',
+          letterSpacing: '-0.01em',
+
+          color: '#000000' }}>Candidate info
         </h2>
       </div>
       <div>
-        <textarea className='testNew' readOnly type="text" value={
+        <textarea className= 'testNew' readOnly type="text" value={
           this.state.candidato.fullName + '\n' +
-          this.state.candidato.email + '\n' +
-          this.state.candidato.telNumber + '\n' +
-          this.state.candidato.url
-        } />
+              this.state.candidato.email + '\n' +
+              this.state.candidato.telNumber + '\n' +
+              this.state.candidato.url
+        }/>
       </div> */}
       <div id='infoCandHR' className='masGrid' style={{ marginLeft: '30px' }}>
         <div>
@@ -131,37 +144,54 @@ class PreSistInterview extends React.Component {
 
       </div> */}
       <div style={{ width: '90%', margin: '20px auto', marginTop: '50px', borderBottom: '2px solid #DE411B' }}></div>
-      <div
-      // style={{
-      // position: 'absolute',
-      // width: '1284px',
-      // height: '72px',
-      // left: '40px',
-      // top: '545px',
+      <div>
+        {/* <div style={{
+        position: 'absolute',
+        width: '1284px',
+        height: '72px',
+        left: '40px',
+        top: '545px',
 
-      // background: 'rgba(155, 180, 190, 0.1)'}}
-      >
+        background: 'rgba(155, 180, 190, 0.1)'
+      }}> */}
         <form onSubmit={this.onChangeCheckbox}>
           <legend style={{ fontSize: '1.5em', color: '#DE411C', textAlign: 'center' }}>Select questions</legend>
-          {this.props.candidateQuestions.map((preg, i = 0) => (
-            <div key={i++} style={{ width: '90%', margin: '30px auto' }}>
+          {this.props.candidateQuestions.map(preg => (
+            <div key={preg.id} style={{ width: '90%', margin: '30px auto' }} >
               <input type="checkbox" name={preg.id} value={preg.id} style={{ transform: 'scale(2)', marginRight: '1%' }}></input>
               <label style={{ fontSize: '1.3em', display: 'inline' }}>{preg.content}</label>
             </div>
           ))}
-          {!this.state.submited && this.props.candidate.status
-            ? <div style={{ width: '50%', margin: '30px auto' }}> <button style={{ width: '100%' }} className='ActionsBotonesNaranja' onClick={() => this.setState({ submited: true })} >Create Interview</button></div>
-            : <div className='halfGrid' >
-              <Link to={'/candidates/' + this.state.candidato.id} style={{ width: '60%', margin: '30px auto' }} ><button style={{ width: '100%' }} className='ActionsBotonesBlanco'> Go Back </button></Link>
-              <div style={{ width: '100%' }}><button onClick={this.onClickFunc} style={{ width: '60%', padding:'25px', margin:'35px auto' }} className='ActionsBotonesNaranja'>Go to interview</button></div>
-            </div>
-          }
+          {/* <div className='halfGrid'> */}
+          <button className='ActionsBotonesNaranja'
+            style={{
+              display: 'block',
+              width: '60%',
+              margin: '30px auto'
+            }}
+          >Create interview</button>
+          <div>
+            {/* <button onClick={this.onClickFunc}
+                className='ActionsBotonesNaranja'
+                // style={{
+                //   position: 'absolute',
+                //   width: '268px',
+                //   height: '60px',
+                //   left: '400px',
+                //   top: '1000px',
+                //   background: '#DE411C',
+                //   border: '1px solid #DE411C',
+                //   boxSizing: 'border-box',
+                //   borderRadius: '30px'
+                // }}
+                >Go to interview</button> */}
+          </div>
+          {/* </div> */}
         </form>
       </div>
-    </div >);
+    </div>);
   }
 }
-
 const mapStateToProps = (state) => ({
   candidate: state.candidate.candidate,
   candidateQuestions: state.question.candidateQuestions
